6daa8ef8a134c4901cb4f0324a83ce4c5784a71d
Caroline Morlet <carolinemorlet@gmail.com>
2021-02-01 14:52:19 +0100

    quest react 5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './styles.css';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className="App">
-      <Contact />
+      <ContactList />
     </div>
   );
 }
